bdb52e230d85ea49563957f8c7e68c4fc50fb472
Lana-mm <malimonovas@list.ru>
2024-10-15 18:03:10 +0500

    end1

--- main.py
+++ main.py
@@ -1,6 +1,39 @@
 import requests
 from bs4 import BeautifulSoup
-url = "https://randomword.com/"
-response = requests.get(url)
 
-print(response.text)
+def get_english_worlds():
+    url = "https://randomword.com/"
+    try:
+        response = requests.get(url)
+
+        soup = BeautifulSoup(response.content, "html.parser")
+        english_worlds = soup.find("div", id="random_word").text.strip()
+        word_definition = soup.find("div", id="random_word_definition").text.strip()
+
+        return {
+            "english_words": english_worlds,
+            "word_definition": word_definition
+        }
+
+    except:
+        print("Произошла ощибка")
+
+def word_game():
+    print("Добро пожаловать в игру!")
+    while True:
+        word_dict = get_english_worlds()
+        word = word_dict.get("english_words")
+        word_definition = word_dict.get("word_definition")
+        print(f"Значение слова - {word_definition}")
+        user = input("Что это за слово?")
+        if user == word:
+            print("Ответ верный")
+        else:
+            print(f"Ответ неверный, было загадано слово - {word}")
+
+        play_again = input("Хотите сыграть еще раз?")
+        if play_again != "Y":
+            print("Спасибо за игру!")
+            break
+word_game()
+
